12bda98502f78782263a1af47b08886e86b2f1b4
zhouxuan19 <xuanzhou19@creditease.cn>
2020-08-06 09:29:08 +0800

    优化

--- src/main/java/com/zx/service/IntegralService.java
+++
@@ -1,9 +0,0 @@
-package com.zx.service;
-
-/**
- * @author zhouxuan
- * @date 2020/8/5 17:01
- */
-public interface IntegralService {
-
-}
--- src/main/java/com/zx/service/impl/IntegralServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.zx.service.impl;
-
-/**
- * @author zhouxuan
- * @date 2020/8/5 17:01
- */
-public class IntegralServiceImpl {
-
-}
